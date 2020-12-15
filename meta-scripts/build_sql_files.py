12b220c233e46663eb1302d0a129edca86648e40
Ian Dees <ian.dees@gmail.com>
2020-12-14 20:09:58 -0600

    Add 2019 5yr config

--- meta-scripts/build_sql_files.py
+++ meta-scripts/build_sql_files.py
@@ -178,6 +178,11 @@ config = {
         'line_number_column_name': 'Line Number',
         'table_id_column_name': 'Table ID'
     },
+    'acs2019_5yr': {
+        'sequence_number_column_name': 'Sequence Number',
+        'line_number_column_name': 'Line Number',
+        'table_id_column_name': 'Table ID'
+    },
 }
 
 if which_acs not in config:
