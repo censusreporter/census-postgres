e037427ec701f4203a050fad3f014b1d43e08b5a
Joe Germuska <joe@germuska.com>
2023-08-24 14:18:33 -0500

    try dropping a different way

--- acs2021_1yr_tables/drop_schema_tables.sql
+++ acs2021_1yr_tables/drop_schema_tables.sql
@@ -2,7 +2,7 @@ do $$ declare
         r record;
     begin
         for r in (select tablename from pg_tables where schemaname = 'acs2021_1yr_tables') loop
-            execute 'drop table if exists ' || quote_ident(r.tablename) || ' cascade';
+            execute 'drop table if exists acs2021_1yr_tables.' || quote_ident(r.tablename) || ' cascade';
         end loop;
     end $$;
     
