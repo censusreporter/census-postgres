865367872e0f5d900c930c4a3382a5898f50eb1e
Ian Dees <ian.dees@gmail.com>
2024-10-30 20:12:25 -0500

    Add ACS 2023 1 yr

---
+++ acs2023_1yr/drop_schema_tables.sql
@@ -0,0 +1,15 @@
+
+    do $$ declare
+        r record;
+        i integer := 0;
+
+    begin
+        for r in (select tablename from pg_tables where schemaname = 'acs2023_1yr') loop
+            execute 'drop table if exists acs2021_1yr.' || quote_ident(r.tablename) || ' cascade';
+            i = i + 1;
+            IF i % 100 = 0 THEN
+                COMMIT;
+            END IF;
+        end loop;
+    end $$;
+    
